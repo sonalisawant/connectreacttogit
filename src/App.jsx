d50c683007b7244bd5160a95439a2d0b36cead41
sonalisawantfullstack <sonalisawantfullstack@gmail.com>
2024-06-20 06:35:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hero from './Hero'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      
+      <Hero/>
+         </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <h2>
+      Hello Hero
+    </h2>
+  )
+}
+
+export default Hero
